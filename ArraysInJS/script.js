2b69dbb722bf71c3ef1b722ed15232ec2ba18c63
n1o2h <aythmadnhylt@gmail.com>
2025-12-09 19:56:26 +0100

    Partie 9 : Mini projet : CRUD en tableau dom + js

--- ArraysInJS/script.js
+++ ArraysInJS/script.js
@@ -1,4 +1,23 @@
-let TableauProduit = [];
+let TableauProduit = [
+      {
+      id: 1,
+      nom: "babane",
+      prix: 23,
+      stock: 18
+      },
+      {
+      id: 2,
+      nom: "Appel",
+      prix: 73,
+      stock: 80
+      },
+      {
+      id: 3,
+      nom: "babane",
+      prix: 23,
+      stock: 20
+      }
+];
 let lastId =0;
 let mood = "create";
 let tmp;
@@ -8,29 +27,6 @@ function generateId(){
       if(TableauProduit.length == 0) return 1;
       return TableauProduit[TableauProduit.length -1].id +1;
 }
-document.forms["AjoutProduit"].addEventListener("submit", (event)=>{
-      event.preventDefault();
-      let form = event.target;
-      let id = generateId();
-            let produit = {
-                  id: id,
-                  nom: form.nomProduit.value,
-                  prix: form.prixProduit.value,
-                  stock: form.stockProduit.value
-            }
-      if(mood == "create"){
-      resetForm()
-      TableauProduit.push(produit);
-      }else{
-            TableauProduit[tmp] = produit;
-            resetForm()
-            mood ="create";
-            submit.innerHTML="Ajouter"
-      }
-      renderCardProduit(TableauProduit);
-      console.log(TableauProduit);
-});
-
 
 function resetForm(){
       document.forms["AjoutProduit"].nomProduit.value = "";
@@ -48,15 +44,14 @@ function renderCardProduit(TableauProduit){
             })
       document.querySelectorAll(".updateBtn").forEach(btn =>{
             btn.addEventListener("click", ()=>{
-                  // document.forms["updateProduit"].classList.remove("d-none");
-                  // document.forms["updateProduit"].classList.add("d-block");
-                  // document.forms["AjoutProduit"].classList.add("d-none");
                   let idProduitBtnToUpdate= btn.getAttribute("data-id");
                   updateProductById(TableauProduit, idProduitBtnToUpdate);
             })
       })
+
 })
 }
+
 function updateProductById(TableauProduit, id){
       let updatedProduct = TableauProduit.find(produit => produit.id == id);
       tmp = TableauProduit.findIndex(produit => produit.id == id);
@@ -67,22 +62,6 @@ function updateProductById(TableauProduit, id){
       form.stockProduit.value = updatedProduct.stock;
       submit.innerHTML="Update";
       mood = "update";
-      console.log(form.nomProduit.value);
-      console.log(form.prixProduit.value);
-
-      console.log(form.stockProduit.value);
-
-      // document.forms["updateProduit"].addEventListener("submit", (event)=>{
-      //       // event.preventDefault();
-      //       let form = event.target;
-      //       console.log(form.nomProduit);
-      //       form.nomProduit.value = updatedProduct.nom
-      //       form.prixProduit.value = updatedProduct.prix
-      //       form.stockProduit.value = updatedProduct.stock;
-      // })
-      // console.log(form);
-
-
 }
 
 function delateProduitById(TableauProduit, id){
@@ -91,6 +70,7 @@ function delateProduitById(TableauProduit, id){
       renderCardProduit(TableauProduit);
 
 }
+
 function renderListPruits(TableauProduit){
       let cardProduit = "";
       TableauProduit.map(produit =>{
@@ -99,6 +79,7 @@ function renderListPruits(TableauProduit){
       
       return cardProduit;
 }
+
 function renderProduit(produit){
       return `<tr>
                   <td>${produit.id}</td>
@@ -110,8 +91,68 @@ function renderProduit(produit){
             </tr>`
 }
 
+function searchProduct(){
+document.forms["searchProduct"].addEventListener("input", (event) =>{
+      event.preventDefault();
+      let searched = event.target.value.trim().toLowerCase();
+      let filtredProduct = TableauProduit.filter(produit => produit.nom.trim().toLowerCase().includes(searched));
+      if(filtredProduct.length == 0 || TableauProduit.length == 0){
+            console.log("aucun produit n'extst avec ce nom");
+      }
+      renderCardProduit(filtredProduct);
+})
+}
+
+function initApp(){
+      
+      if(TableauProduit.length != 0){
+            renderCardProduit(TableauProduit);
+      }
+      document.forms["AjoutProduit"].addEventListener("submit", (event)=>{
+      event.preventDefault();
+      let form = event.target;
+            let produit = {
+                  nom: form.nomProduit.value,
+                  prix: form.prixProduit.value,
+                  stock: form.stockProduit.value
+            }
+      if(mood == "create"){
+            let id = generateId();
+            produit.id = id;
+            resetForm()
+            TableauProduit.push(produit);
+      }else{
+            TableauProduit[tmp] = produit;
+            produit.id = tmp + 1;
+            resetForm()
+            mood ="create";
+            submit.innerHTML="Ajouter"
+      }
+      renderCardProduit(TableauProduit);
+      });
+      searchProduct();
 
 
 
+      document.getElementById("prixSort").addEventListener("click", ()=> {
+            document.getElementById("typeOfSort").innerHTML = `Les produits tries par prix croissant`;
+            let ProductSortByPrice = TableauProduit.sort((a, b) => a.prix-b.prix);
+            renderCardProduit(ProductSortByPrice);
+      });
+
+      document.getElementById("qauntiteSort").addEventListener("click", ()=> {
+            document.getElementById("typeOfSort").innerHTML = `Les produits tries par quantite croissant`;
+
+            let ProductSortByQuantite = TableauProduit.sort((a, b) => b.stock-a.stock);
+            renderCardProduit(ProductSortByQuantite);
+      });
+      document.getElementById("noSort").addEventListener("click", ()=> {
+            document.getElementById("typeOfSort").innerHTML = ``;
+            let ProductSortBy=Id = TableauProduit.sort((a, b) => a.id-b.id);
+            renderCardProduit(ProductSortBy);
+      }
+);
+}
+initApp();
 
 
